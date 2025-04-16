c30d6c4a9f841138e09ed1352f82aa9dff2360bf
VivaPen <90327284+VivaPen@users.noreply.github.com>
2025-04-16 06:48:35 +0000

    Removing duplicate lines

--- linux-script.sh
+++ linux-script.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
+
 if [ "$(id -u)" -ne 0 ]; then
-    echo "Please run this script as root."
+    echo "Please run this script as root."
+    exit 1
+fi
 
-fi
-
-if [ ! -e /usr/local/ASR/Vx/etc/RcmProtectionState.json ]; then
-    echo "Protection state healthy."
 if [ ! -e /usr/local/ASR/Vx/etc/RcmProtectionState.json ]; then
 
-    echo "Protection state healthy."
-rm -f /usr/local/ASR/Vx/etc/RcmProtectionState.json
-
+    echo "File not found, Protection state healthy."
+    exit 0
 fi
-/usr/local/ASR/Vx/bin/start
 
-
-/usr/local/ASR/Vx/bin/start
 /usr/local/ASR/Vx/bin/stop
 
 rm /usr/local/ASR/Vx/etc/RcmProtectionState.json 
