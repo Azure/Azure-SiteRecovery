441e9af839c3dcdfdf668e2206cc6fc4e3535bd7
VivaPen <90327284+VivaPen@users.noreply.github.com>
2025-04-16 10:57:55 +0530

    Fix script formatting and add force remove flag

--- linux-script.sh
+++ linux-script.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 if [ "$(id -u)" -ne 0 ]; then
-    echo "Please run this script as root."
-    exit 1
+    echo "Please run this script as root."
+    exit 1
 fi
 
 if [ ! -e /usr/local/ASR/Vx/etc/RcmProtectionState.json ]; then
     echo "Protection state healthy."
-    exit 0
+    exit 0
 fi
 
+
+fi
+rm -f /usr/local/ASR/Vx/etc/RcmProtectionState.json 
+
+/usr/local/ASR/Vx/bin/start
 /usr/local/ASR/Vx/bin/stop
 
 rm /usr/local/ASR/Vx/etc/RcmProtectionState.json 
