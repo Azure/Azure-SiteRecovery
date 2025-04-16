9cfaa1827313968409806d111e17c7e097d15d67
VivaPen <90327284+VivaPen@users.noreply.github.com>
2025-04-16 10:45:17 +0530

    Add exit command if file not found

--- windows-script.ps1
+++ windows-script.ps1
@@ -1,11 +1,27 @@
 
+
 $filePath = "C:\Program Files (x86)\Microsoft Azure Site Recovery\agent\Application Data\etc\RcmProtectionState.json"
+
 if ( -not (Test-Path $filePath) )
+
 {
+
     Write-Host "file $filePath not found."
+    exit
 }
 
 
+
+
+
+    Write-Host "file $filePath not found."
+
+}
+
+
+
+
+
 Stop-Service svagents
 
 cd "C:\Program Files (x86)\Microsoft Azure Site Recovery\agent\Application Data\etc"
