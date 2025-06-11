3a2c4aa9040f60b351302a8c235f48229be856ec
VivaPen <90327284+VivaPen@users.noreply.github.com>
2025-06-11 14:23:38 +0530

    Adding download links and rearranged the content (#12)

---
+++ TroubleShootingGuides/IssueId-5NFG-PXG/windows-script.ps1
@@ -0,0 +1,15 @@
+$filePath = "C:\Program Files (x86)\Microsoft Azure Site Recovery\agent\Application Data\etc\RcmProtectionState.json"
+
+if ( -not (Test-Path $filePath) )
+{
+    Write-Host "file $filePath not found."
+    exit
+}
+
+Stop-Service svagents
+
+cd "C:\Program Files (x86)\Microsoft Azure Site Recovery\agent\Application Data\etc"
+
+del RcmProtectionState.json
+
+Start-Service svagents
